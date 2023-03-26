ef3010bd9b771a1d92fb580c7816725480e77c7f
Lil-Adonis <adenugaib24@gmail.com>
2023-03-26 09:25:03 -1100

    :tada: started Header build
    Next cofig set  for images

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <h1>I am a header</h1>
+      {/* Left */}
 
-export default Header
+      <div>
+        <Image src="https://links.papareact.com/ocw" fill/>
+      </div>
+
+      {/* Middle */}
+
+      {/* RIght */}
+    </div>
+  );
+};
+
+export default Header;
