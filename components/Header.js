c9549a97c69276db4c73cffeb554b2c109f58aea
Lil-Adonis <adenugaib24@gmail.com>
2023-03-28 00:42:10 -1100

    :thread: Globalcss
    custom css

--- components/Header.js
+++ components/Header.js
@@ -7,12 +7,13 @@ import {
     HeartIcon,
     PaperAirplaneIcon,
     MenuIcon,
+    HomeIcon,
 } from "@heroicons/react/outline"
 
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-between max-w-6xl">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* Left */}
         <div className="relative hidden lg:inline-grid   w-24 cursor-pointer">
           <Image src="https://links.papareact.com/ocw" alt="logo" fill style={{objectFit:"contain"}}/>
@@ -23,15 +24,24 @@ const Header = () => {
         </div>
 
         {/* Middle */}
-
+        <div className="max-w-xs">
         <div className="mt-1 relative p-3 rounded-md">
             <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
                <SearchIcon className="h-5 w-5 text-gray-500"/>
             </div>
-            <input className="bg-gray-50 block w-full pl-10 sm:text-sm rounded-md border-gray-300 focus:border-black focus:ring-black " type="text" placeholder="search"/>
+            <input 
+            className="bg-gray-50 block w-full pl-10 sm:text-sm 
+            rounded-md border-gray-300 focus:border-black 
+            focus:ring-black " type="text" placeholder="search"/>
+        </div>
         </div>
 
         {/* RIght */}
+        <div className="flex items-center justify-end space-x-4">
+        <HomeIcon className="h-10 w-10"/>
+        <MenuIcon className="h-6 md:hidden cursor-pointer"/>
+        <PaperAirplaneIcon className="h-10 w-10"/>
+        </div>
       </div>
     </div>
   );
