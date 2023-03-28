f3492418582eab4a1272e10bebe4dac8d6d7b550
Lil-Adonis <adenugaib24@gmail.com>
2023-03-28 00:18:10 -1100

    :memo: tailwindcss forms
    input field

--- components/Header.js
+++ components/Header.js
@@ -14,21 +14,21 @@ const Header = () => {
     <div>
       <div className="flex justify-between max-w-6xl">
         {/* Left */}
-        <div className="relative hidden lg:inline-grid  h-24 w-24 cursor-pointer">
+        <div className="relative hidden lg:inline-grid   w-24 cursor-pointer">
           <Image src="https://links.papareact.com/ocw" alt="logo" fill style={{objectFit:"contain"}}/>
         </div>
 
-        <div className="relative flex-shrink-0 lg:hidden h-10 w-10 cursor-pointer">
+        <div className="relative flex-shrink-0 lg:hidden w-10 cursor-pointer">
           <Image src="https://links.papareact.com/jjm" alt="logo" fill style={{objectFit:"contain"}}/>
         </div>
 
         {/* Middle */}
 
-        <div>
-            <div>
+        <div className="mt-1 relative p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
                <SearchIcon className="h-5 w-5 text-gray-500"/>
             </div>
-            <input type="text" placeholder="search"/>
+            <input className="bg-gray-50 block w-full pl-10 sm:text-sm rounded-md border-gray-300 focus:border-black focus:ring-black " type="text" placeholder="search"/>
         </div>
 
         {/* RIght */}
