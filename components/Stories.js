7571db870d915be921d11c0d71a4316ab8a3c7bc
Lil-Adonis <adenugaib24@gmail.com>
2023-03-30 11:14:09 -1100

    :memo: Stories, feed under build
    installed Api and added some codes

--- components/Stories.js
+++ components/Stories.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { faker } from '@faker-js/faker';
+import { useEffect } from 'react';
+
+
 
 const Stories = () => {
+    const [suggestions, setSuggestions] = useState([]);
+
+   useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+        ...faker.helpers.contextualCard(),
+        id: i,
+    }));
+    setSuggestions(suggestions);
+   }, []);
+
+
+
   return (
-    <div>Stories</div>
+    <div>
+        {suggestions.map(profile => (
+            
+        ))}
+        {/* Stories */}
+        {/* Stories */}
+        {/* Stories */}
+        {/* Stories */}
+        {/* Stories */}
+        {/* Stories */}
+    </div>
   )
 }
 
