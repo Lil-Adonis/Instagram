0be5c9057f0a0d23b360ef247811c3a254999fe9
Lil-Adonis <adenugaib24@gmail.com>
2023-03-31 04:39:42 -1100

    :construction: added packages
    packages added and feed improved

--- components/Story.js
+++ components/Story.js
@@ -3,10 +3,16 @@ import React from 'react'
 function Story ({ img, username }) {
   return (
     <div>
-        <img src={img} alt='feed'/>
-        <p>{username}</p>
+      <img
+        className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2
+        cursor-pointer object-contain hover:scale-110 
+        transition-transform
+        duration-200 ease-out"
+        src={img}
+        alt=""
+      />
+      <p className="text-xs w-14 truncate text-center">{username}</p>
     </div>
   );
-}
-
+};
 export default Story;
