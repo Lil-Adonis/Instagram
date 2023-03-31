c3227068d932268dad7f82b41912947704b84b8e
Lil-Adonis <adenugaib24@gmail.com>
2023-03-31 04:10:41 -1100

    :bug: fixed faker
    fixed faker and update useEffect

--- components/Stories.js
+++ components/Stories.js
@@ -1,35 +1,32 @@
-import React from 'react'
-import { faker } from '@faker-js/faker';
-import { useEffect } from 'react';
-
-
+import React from 'react';
+import { faker } from "@faker-js/faker";
+import Story from './Story';
+import { useEffect, useState } from 'react';
 
 const Stories = () => {
-    const [suggestions, setSuggestions] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
 
-   useEffect(() => {
+  useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
-        ...faker.helpers.contextualCard(),
-        id: i,
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      avatar: faker.image.avatar(),
+      id: i,
     }));
     setSuggestions(suggestions);
-   }, []);
-
-
+  }, []);
 
   return (
     <div>
-        {suggestions.map(profile => (
-            
-        ))}
-        {/* Stories */}
-        {/* Stories */}
-        {/* Stories */}
-        {/* Stories */}
-        {/* Stories */}
-        {/* Stories */}
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Stories
+export default Stories;
